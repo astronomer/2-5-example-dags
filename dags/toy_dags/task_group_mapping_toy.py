56f2b8848dba1053aeb5dea8a8d25dd7cbde74e6
TJaniF <tamara.fingerlin@astronomer.io>
2022-12-11 03:16:15 +0100

    toy DAG mapped task groups

--- dags/toy_dags/task_group_mapping_toy.py
+++ dags/toy_dags/task_group_mapping_toy.py
@@ -1,7 +1,5 @@
 from airflow import DAG
-from airflow.utils.task_group import TaskGroup
-from airflow.decorators import task_group
-from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task_group, task
 from pendulum import datetime
 
 with DAG(
@@ -11,18 +9,36 @@ with DAG(
     catchup=False
 ):
 
-    with TaskGroup(group_id="group1") as tg1:
-        t1 = EmptyOperator(task_id="t1")
-        t2 = EmptyOperator(task_id="t2")
-
-        t1 >> t2
-
-
+    # the task group
     @task_group(
         group_id="group2"
     )
-    def tg2():
-        t3 = EmptyOperator(task_id="t3")
-        t4 = EmptyOperator(task_id="t4")
+    def tg2(my_num):
 
-        t3 >> t4
+        @task
+        def print_num(num):
+            return num
+        
+        @task
+        def add_42(num):
+            return num + 42
+        
+        print_num(my_num) >> add_42(my_num)
+
+    # a downstream task to print out resulting XComs
+    @task
+    def pull_xcom(**context):
+
+        pulled_xcom = context["ti"].xcom_pull(task_ids=['group2.add_42'], key="return_value")
+
+        # will print out a list of all results from the add_42 task in all mapped
+        # task instances of the task group
+        print(pulled_xcom)
+
+    # creating 6 mapped task instances of the TaskGroup tg2
+    tg_object = tg2.expand(my_num=[19, 23, 42, 8, 7, 108])
+
+    # setting dependencies
+    tg_object >> pull_xcom()
+
+    
