093e407580f64e4a553b2f9df96045c90a4c70e4
TJaniF <tamara.fingerlin@astronomer.io>
2022-12-11 02:05:49 +0100

    datasets and taskflow use case example

--- dags/datasets_and_taskflow/downstream_datasets_taskflow_usecase.py
+++ dags/datasets_and_taskflow/downstream_datasets_taskflow_usecase.py
@@ -1,14 +1,50 @@
-from airflow import DAG
+from airflow import DAG, Dataset
 from airflow.decorators import task
 from pendulum import datetime
-from airflow.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.s3 import (
+    S3ListOperator, S3CopyObjectOperator
+)
 
+MY_S3_BUCKET = "myexamplebucketone"
+MY_FIRST_FOLDER = "ex1/"
+MY_S3_BUCKET_DELIMITER = "/"
+MY_FILENAME="my_message.txt"
+AWS_CONN_ID = "aws_connection"
+
+MY_S3_BUCKET_TO_COPY_TO = "myexamplebuckettwo"
+
+my_dataset = Dataset(
+    f"s3://{MY_S3_BUCKET}{MY_S3_BUCKET_DELIMITER}{MY_FIRST_FOLDER}{MY_FILENAME}"
+)
 
 with DAG(
     dag_id="downstream_datasets_taskflow_usecase",
     start_date=datetime(2022, 12, 1),
-    schedule=None,
-    catchup=False
+    schedule=[my_dataset],
+    catchup=False,
+    tags=["datasets", "taskflow", "usecase"]
 ):
 
-    t1 = EmptyOperator(task_id="t1")
+    # list all existing files in MY_FIRST_FOLDER in MY_S3_BUCKET
+    list_files = S3ListOperator(
+        task_id=f"list_files",
+        aws_conn_id=AWS_CONN_ID, 
+        bucket=MY_S3_BUCKET,
+        prefix=MY_FIRST_FOLDER,
+        delimiter=MY_S3_BUCKET_DELIMITER
+    )
+
+    # copy all files to MY_S3_BUCKET_TO_COPY_TO
+    copy_files = S3CopyObjectOperator.partial(
+        task_id="copy_files",
+        aws_conn_id=AWS_CONN_ID, 
+    ).expand_kwargs(
+        list_files.output.map(
+            lambda x: {
+                "source_bucket_key" : f"s3://{MY_S3_BUCKET}{MY_S3_BUCKET_DELIMITER}{x}",
+                "dest_bucket_key" : f"s3://{MY_S3_BUCKET_TO_COPY_TO}{MY_S3_BUCKET_DELIMITER}{x}"
+            }
+        )
+    )
+
+    list_files >> copy_files
--- dags/datasets_and_taskflow/upstream_datasets_taskflow_usecase.py
+++ dags/datasets_and_taskflow/upstream_datasets_taskflow_usecase.py
@@ -1,14 +1,71 @@
-from airflow import DAG
+from airflow import DAG, Dataset
 from airflow.decorators import task
 from pendulum import datetime
-from airflow.operators.empty import EmptyOperator
+from airflow.providers.amazon.aws.operators.s3 import (
+    S3ListPrefixesOperator, S3CreateObjectOperator
+)
 
+MY_S3_BUCKET = "myexamplebucketone"
+MY_S3_FOLDER_PREFIX = "e"
+MY_S3_BUCKET_DELIMITER = "/"
+MY_DATA="Hi S3 bucket!"
+MY_FILENAME="my_message.txt"
+AWS_CONN_ID = "aws_connection"
 
 with DAG(
     dag_id="upstream_datasets_taskflow_usecase",
     start_date=datetime(2022, 12, 1),
     schedule=None,
-    catchup=False
+    catchup=False,
+    tags=["datasets", "taskflow", "usecase"]
 ):
 
-    t1 = EmptyOperator(task_id="t1")
+    # list all root level folders in my S3 bucket that start with my prefix
+    list_folders_in_bucket = S3ListPrefixesOperator(
+        task_id="list_folders_in_bucket",
+        aws_conn_id=AWS_CONN_ID,
+        bucket=MY_S3_BUCKET,
+        prefix=MY_S3_FOLDER_PREFIX,
+        delimiter=MY_S3_BUCKET_DELIMITER
+    )
+
+    # create a separate Dataset object for each of the folders
+    @task
+    def create_datasets_from_s3_folders(folder_list):
+
+        datasets_lists = []
+        for folder in folder_list:
+
+            uri =  f"s3://{MY_S3_BUCKET}{MY_S3_BUCKET_DELIMITER}{folder}{MY_FILENAME}"
+            datasets_lists.append(Dataset(uri))
+
+        print(f"These datasets were created: {datasets_lists}")
+
+        # returning a list of Datasets (2.5 feature)
+        return datasets_lists
+
+
+    list_of_datasets = create_datasets_from_s3_folders(
+            list_folders_in_bucket.output
+    )
+
+    # write MY_DATA into a new MY_FILENAME in each of the S3 folders
+    write_file_to_S3 = S3CreateObjectOperator.partial(
+        task_id="write_file_to_S3",
+        aws_conn_id=AWS_CONN_ID,
+        data=MY_DATA,
+        replace=True
+    ).expand(
+        s3_key=list_of_datasets.map(lambda x: x.uri) # retrieving the keys from the Datasets
+    )
+
+
+    # since outlets is not a mappable parameter, use a task flow task to produce to the datasets
+    # this too is only possible in 2.5+
+    @task
+    def produce_to_datasets(dataset_obj):
+        return dataset_obj
+
+    write_file_to_S3 >> produce_to_datasets.expand(dataset_obj=list_of_datasets)
+
+    
