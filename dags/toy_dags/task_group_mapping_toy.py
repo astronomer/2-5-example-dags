5f3555e1c09085d88d5aee02c4e6e398034cacd3
TJaniF <tamara.fingerlin@astronomer.io>
2022-12-11 17:26:16 +0100

    add mapped task group example

--- dags/toy_dags/task_group_mapping_toy.py
+++ dags/toy_dags/task_group_mapping_toy.py
@@ -6,12 +6,13 @@ with DAG(
     dag_id="task_group_mapping_toy",
     start_date=datetime(2022, 12, 1),
     schedule=None,
-    catchup=False
+    catchup=False,
+    tags=["task_group", "toy", "dynamic_task_mapping"]
 ):
 
     # the task group
     @task_group(
-        group_id="group2"
+        group_id="group1"
     )
     def tg2(my_num):
 
@@ -29,13 +30,16 @@ with DAG(
     @task
     def pull_xcom(**context):
 
-        pulled_xcom = context["ti"].xcom_pull(task_ids=['group2.add_42'], key="return_value")
+        pulled_xcom = context["ti"].xcom_pull(
+            task_ids=['group1.add_42'], # reference a task in a taskgroup with task_group_id.task_id
+            map_indexes=[2,3], # only pull xcom from specific mapped task instances (2.5 feature)
+            key="return_value"
+        )
 
-        # will print out a list of all results from the add_42 task in all mapped
-        # task instances of the task group
+        # will print out a list of results from map index 2 and 3 of the add_42 task 
         print(pulled_xcom)
 
-    # creating 6 mapped task instances of the TaskGroup tg2
+    # creating 6 mapped task instances of the TaskGroup tg2 (2.5 feature)
     tg_object = tg2.expand(my_num=[19, 23, 42, 8, 7, 108])
 
     # setting dependencies
